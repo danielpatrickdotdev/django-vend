15e2affe1ca0d2fd6d507ba49827222976f2549c
remarkablerocket <danny@justaboutanywhere.co.uk>
2016-12-18 21:33:38 +0000

    Try to get pip to automatically install requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from setuptools import find_packages, setup
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-vend',
     version='0.1',
+    install_requires=requirements,
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
