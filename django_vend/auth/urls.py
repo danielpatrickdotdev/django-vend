2dbb0f86f1bb1e3f6282841992a16c2c1b306b5a
Daniel Patrick <danieljudepatrick@gmail.com>
2016-12-20 19:09:47 +0000

    PEP8

--- django_vend/auth/urls.py
+++ django_vend/auth/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.VendAuthLogin.as_view(), name='vend_auth_login'),
-    url(r'^complete/$', views.VendAuthComplete.as_view(), name='vend_auth_complete'),
-    url(r'^select-user/$', views.VendAuthSelectUser.as_view(), name='vend_auth_select_user'),
+    url(r'^login/$', views.VendAuthLogin.as_view(),
+        name='vend_auth_login'),
+    url(r'^complete/$', views.VendAuthComplete.as_view(),
+        name='vend_auth_complete'),
+    url(r'^select-user/$', views.VendAuthSelectUser.as_view(),
+        name='vend_auth_select_user'),
 ]
