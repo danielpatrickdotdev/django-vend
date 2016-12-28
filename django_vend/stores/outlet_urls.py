e83266987db962f2546da84f5f507ff4f67e3499
remarkablerocket <danny@justaboutanywhere.co.uk>
2016-12-28 17:36:28 +0000

    Add urlconf entry for VendOutlet detail

--- django_vend/stores/outlet_urls.py
+++ django_vend/stores/outlet_urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.OutletList.as_view(),
         name='vend_outlet_list'),
+    url(r'^(?P<uid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', views.OutletDetail.as_view(),
+        name='vend_outlet_detail'),
 ]
