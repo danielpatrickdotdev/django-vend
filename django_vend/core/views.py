6f2adf06d9f0385305472f200af06d869db3e9b3
remarkablerocket <danny@justaboutanywhere.co.uk>
2016-12-28 10:59:05 +0000

    Create some base views which call manager's synchronise function every time data is retrieved

--- django_vend/core/views.py
+++ django_vend/core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class VendAuthMixin(LoginRequiredMixin):
+    def get_queryset(self):
+        retailer = self.request.user.vendprofile.retailer
+        return self.model.objects.filter(retailer=retailer)
+
+
+class VendAuthSingleObjectSyncMixin(VendAuthMixin):
+    def get_object(self):
+        retailer = self.request.user.vendprofile.retailer
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        self.model.objects.synchronise(retailer, pk)
+        return super(VendAuthSingleObjectSyncMixin, self).get_object()
+
+
+class VendAuthCollectionSyncMixin(VendAuthMixin):
+    def get_queryset(self):
+        retailer = self.request.user.vendprofile.retailer
+        self.model.objects.synchronise(retailer)
+        return super(VendAuthCollectionSyncMixin, self).get_queryset()
+
